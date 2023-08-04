f6ecea0e87c0123aa18c0839dd106798f77ca7fc
bautomaton <b.bialobrzycki@gmail.com>
2023-08-04 23:14:34 +0100

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
-
+//test comment
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
